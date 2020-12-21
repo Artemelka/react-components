0bab126bda2f165ec672cf895817d417de7c581f
Artemelka <emelyanov.artem@wildberries.ru>
2020-12-21 13:42:48 +0300

    remove unused constants

--- src/elements/constants.ts
+++ src/elements/constants.ts
@@ -8,52 +8,8 @@ export const PAGE_MODAL_CONTAINER_CLASS_NAME = 'Page__modal-container';
 export const PAGE_NOTIFICATION_CONTAINER_CLASS_NAME = 'Page__notification-container';
 export const TAG_NAME_DIV = 'div';
 
-// URL
-export const PROJECT_LINK = 'https://github.com/Artemelka/ReactUiLibrary';
-export const HOME_URL = '/';
-export const LIBRARY_URL = '/library';
-export const INPUT_SUB_URL = '/inputs';
-
-// Store keys
-export enum APP_STORE_KEY {
-  ROUTER = 'router',
-}
-
-// HTTP
-export const DEFAULT_TIMEOUT = 60000;
-export enum REQUEST_METHOD {
-  POST = 'POST',
-  GET = 'GET',
-}
-
-export const HTTP_STATUSES = {
-  PROCESSING: 102,
-  OK: 200,
-  ACCEPTED: 202,
-  NON_AUTHORITATIVE: 203,
-  PARTIAL_CONTENT: 206,
-  MOVED: 301,
-  NOT_MODIFIED: 304,
-  BAD_REQUEST: 400,
-  PAYMENT_REQUIRED: 402,
-  FORBIDDEN: 403,
-  NOT_FOUND: 404,
-  NOT_ACCEPTABLE: 406,
-  TIMEOUT: 408,
-  EXPECTATION_FAILED: 417,
-  UNPROCESSABLE_ENTITY: 422,
-  LOCKED: 423,
-  FAILED_DEPENDENCY: 424,
-  TOO_MANY_REQUESTS: 429,
-  RETRY_WITH: 449,
-  ILLEGAL: 451,
-  INTERNAL_ERROR: 500,
-  UNAVAILABLE: 503,
-  INSUFFICIENT_STORAGE: 507,
-};
-
 // Key code
-export const keyCodes = {
+export const KEY_CODES = {
   ALT: 18,
   BACKSPACE: 8,
   CTRL: 17,
