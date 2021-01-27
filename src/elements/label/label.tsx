a9ad2ac9d09128cc89ceb52da02f3a70693d55c5
Artemelka <emelyanov.artem@wildberries.ru>
2021-01-27 08:49:48 +0300

    add radiogroup component

--- src/elements/label/label.tsx
+++ src/elements/label/label.tsx
@@ -5,13 +5,20 @@ import style from './label.module.scss';
 const cn = classNames.bind(style);
 const CLASS_NAME = 'Label';
 
-type TemplateProps = {
+type LabelProps = {
+  /** Текстовое содержимое лейбла */
   children: string;
+  /** Флаг неактивного состояния */
   disabled?: boolean;
+  /** id инпута для которого предназначен лейбл */
   htmlFor?: string | number;
+  /** Флаг состояния ошибки */
   isError?: boolean;
+  /** Задает отступ лейбла в зависимости от позици расположения */
   position?: 'left' | 'right' |'top' ;
+  /** Задает размер лейбла */
   size?: 'big' | 'medium' | 'small';
+  /** Задает цветовую тему лейбла */
   themeColor?: 'base' | 'accent' | 'main' | 'primary' | 'secondary';
 };
 
@@ -23,7 +30,7 @@ export const Label = memo(({
   position = 'top',
   size = 'medium',
   themeColor = 'main',
-}: TemplateProps) => (
+}: LabelProps) => (
   <label
     className={cn(CLASS_NAME, {
       [`${CLASS_NAME}--clickable`]: htmlFor,
